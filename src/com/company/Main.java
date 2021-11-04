84905b838aee6c250a8ea02453da3cc39077d5fb
LosSantosSavior <nickrussell001@gmail.com>
2021-11-04 10:45:27 -0400

    commit 11/4/21 10:45AM

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,7 +15,7 @@ public class Main {
 	    var dataRequest = requestBuilder.uri(webURI).build();
 		HttpResponse<String> response = null;
 		try	{
-			dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString());
+			response = dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString());
 		} catch	(IOException exceptionObject){
 			System.out.println("Error connecting to site or network is down.");
 		} catch(InterruptedException e)	{
@@ -25,5 +25,7 @@ public class Main {
 			System.out.println("Something went wrong getting data from the website.");
 			System.exit(-1);
 		}
+		var usefulData = response.body();
+		System.out.println(usefulData);
     }
 }
